27de5d4a062c196d992c1442cae95ba9346dc972
Ahmad Irfan Mohammad Shukri <nerve2009@yahoo.com>
2017-08-23 17:37:43 +0800

    create POST request for express

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/node_kb', {useMongoClient: true});
 let db = mongoose.connection;
 
@@ -19,15 +21,19 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // bring the article models
-let article = require('./models/article');
+let Article = require('./models/article');
 
 // load view engine - PUG
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Body Parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 // home route
 app.get('/', (req, res) => {
-	article.find({}, (err, articles) => {
+	Article.find({}, (err, articles) => {
 		if(err){
 			console.error(err);
 		} else {
@@ -44,7 +50,24 @@ app.get('/articles/add', (req, res) => {
 	res.render('add_article', {
 		title: 'Add new article'
 	});
-})
+});
+
+// add submit POST route
+app.post('/articles/add', (req, res) => {
+	let article = new Article();
+	article.title = req.body.title;
+	article.author = req.body.author;
+	article.body = req.body.body;
+
+	article.save((err) => {
+		if(err){
+			console.error(err);
+			return;
+		} else {
+			res.redirect('/');
+		}
+	})
+});
 
 app.listen(port, (req, res) => {
 	console.log('Listening to port ' + port);
