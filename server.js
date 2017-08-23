32fa4e144743a4470eeb39fd7feaa22c96a03ad4
Ahmad Irfan Mohammad Shukri <nerve2009@yahoo.com>
2017-08-23 16:39:15 +0800

    add pug layout page

--- server.js
+++ server.js
@@ -10,8 +10,40 @@ app.set('view engine', 'pug');
 
 // home route
 app.get('/', (req, res) => {
-	res.render('index');
+
+	// hardcoded articles list
+	let articles = [{
+		id: 1,
+		title: 'Article 1',
+		author: 'Irfan',
+		body: 'This is article #1',
+		created_date: Date.now()
+	},{
+		id: 2,
+		title: 'Article 2',
+		author: 'John',
+		body: 'This is article #2',
+		created_date: Date.now()
+	},{
+		id: 3,
+		title: 'Article 3',
+		author: 'Jane',
+		body: 'This is article #3',
+		created_date: Date.now()
+	}];
+
+	res.render('index', {
+		title: 'Articles',
+		articles: articles
+	});
 });
+
+// add route
+app.get('/articles/add', (req, res) => {
+	res.render('add_article', {
+		title: 'Add new article'
+	});
+})
 
 app.listen(port, (req, res) => {
 	console.log('Listening to port ' + port);
